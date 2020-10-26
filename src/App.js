9a8e4e5e1f469add943f1504d33734fd46e285a7
Titivat <titivatmo@gmail.com>
2020-10-27 05:01:33 +0700

    example onClick

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Button from './components/button/Button.jsx';
 
 function App() {
+  const [count, setCount] = useState( 0 );
+
+  function changeValue(){
+    setCount( count + 1 );
+    console.log( count );
+  }
+
   return (
     <div>
+      <h1>{ count }</h1>
+
       <Button name="Apply" 
               size={[50, 50]} 
               textSize={29} 
               bcColour="blue"
               textColour="white"
+              onClick = {changeValue}
       />
-       
+
     </div>
   );
 }
