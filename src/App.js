6fb4f7fa28c58fff01d6327a8b789c96ecf75dc5
Titivat <titivatmo@gmail.com>
2020-10-29 05:33:11 +0700

    main page created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import Button from './components/button/Button.jsx';
-import ForumPage from '../src/pages/ForumPage/ForumPage.jsx'
+import ForumPage from '../src/pages/ForumPage/ForumPage.jsx';
+import MainPage from '../src/pages/mainPage/MainPage.jsx';
 
 function App() {
   return (
     <div>
-      <ForumPage/>
+      <MainPage/>
     </div>
   );
 }
