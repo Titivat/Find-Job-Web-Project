eb1ebd2476a690d6ce50280c7a1a53483e4aa9e5
Titivat <titivatmo@gmail.com>
2020-10-31 15:39:09 +0700

    add  hader

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,42 @@
 import './Header.css'
+import logo from '../../images/logo.png'
 
-function Header(){
+function Header( props ){
+    const { isDisplay } = props 
 
     return(
         <div className="header-container">
             <div className="header-left-item">
-                logo
+                <a><img className="header-left-img" src={logo}></img></a>
+
+                <div className="header-left-text">
+                    <a>job spot </a>
+                    <a> | </a>
+                    <a>about us</a>
+                </div>
             </div>
 
             <div className="header-right-item">
-                <a>sign in</a>
-                <a> | </a>
-                <a>sign up</a>
+                < DisplaySignUp isDisplay = { false } >
+                    { props.children }
+                </DisplaySignUp>
             </div>
         </div>
+    );
+}
+
+function DisplaySignUp( props  ){
+    const { isDisplay } = props
+
+    return ( ( isDisplay )  ? 
+              <div>
+                <a>sign in</a> 
+                <a> &nbsp; | &nbsp; </a>
+                <a>sign up</a>
+              </div>
+              : 
+              <div>{props.children}</div>
+              
     );
 }
 
