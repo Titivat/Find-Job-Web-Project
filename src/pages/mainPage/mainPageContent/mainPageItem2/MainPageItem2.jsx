5010e1c80a1172a7e84bd96bc9323543c21711f2
Titivat <titivatmo@gmail.com>
2020-12-19 11:48:01 +0700

    update: mainpage2

--- src/pages/mainPage/mainPageContent/mainPageItem2/MainPageItem2.jsx
+++ src/pages/mainPage/mainPageContent/mainPageItem2/MainPageItem2.jsx
@@ -2,13 +2,16 @@ import './MainPageItem2.css'
 import enginearImg from '../../../../images/enginearImg.jpg'; 
 import designImg from '../../../../images/designImg.jpg'; 
 import businessImg from '../../../../images/businessImg.jpg'; 
+import businessIcon from '../../../../images/handshake.png'; 
+import designIcon from '../../../../images/designIcon.png'; 
+import enginearIcon from '../../../../images/enginearIcon.png'; 
 
 function MainPageItem2(){
     return(
         <div>
             <div className="main-page-item2-title">
-                <p>Job field</p>
-                <p>discription</p>
+                <p>Job field:</p>
+                <p>pick the file you are interted in</p>
             </div>
 
             <div className="main-page-item2-job-forum-container">
@@ -19,7 +22,8 @@ function MainPageItem2(){
 
                     <div className="main-page-item2-forum-item-description">
                         <p>Engineer</p>
-                        <p>image</p>
+
+                        <img className="main-page-item2-icon" src={enginearIcon} />
                     </div>
                     
                 </div>
@@ -31,7 +35,8 @@ function MainPageItem2(){
 
                     <div className="main-page-item2-forum-item-description">
                         <p>Business</p>
-                        <p>image</p>
+                        
+                        <img className="main-page-item2-icon" src={businessIcon} />
                     </div>
                 </div>
 
@@ -42,7 +47,8 @@ function MainPageItem2(){
                     
                     <div className="main-page-item2-forum-item-description">
                         <p>Design</p>
-                        <p>image</p>
+                        
+                        <img className="main-page-item2-icon" src={designIcon} />
                     </div>
                 </div>
             </div>
