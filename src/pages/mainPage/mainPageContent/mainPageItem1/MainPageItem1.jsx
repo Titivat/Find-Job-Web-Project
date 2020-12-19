af616d490bf2223355b7b37638a5a5b57d791e34
Titivat <titivatmo@gmail.com>
2020-12-19 12:19:49 +0700

    add: mainpageItem1 | background image

--- src/pages/mainPage/mainPageContent/mainPageItem1/MainPageItem1.jsx
+++ src/pages/mainPage/mainPageContent/mainPageItem1/MainPageItem1.jsx
@@ -1,13 +1,23 @@
 import './MainPageItem1.css';
+import ellipse1 from '../../../../images/ellipse1.png';
 
 function MainPageItem1(){
     return(
         <div>
             <div className="main-page-item1-container">
                 <div className="main-page-left-item">
-                    <p className="main-page-left-item-background-img">image in the background</p>
-                    <p>long text quote</p>
-                    <p>Confucius</p>
+                    <img className="main-page-left-item-background-img" src={ellipse1} >
+                    </img>
+
+                    <p className="main-page-left-text">
+                        Choose a job you love,
+                        <p>and you will never have to </p>
+                        work a day in your life.
+
+                        <p className="main-page-left-buttom-text">-Confucius</p>
+                    </p>
+
+                    
                 </div>
 
                 <div className="main-page-right-item">
