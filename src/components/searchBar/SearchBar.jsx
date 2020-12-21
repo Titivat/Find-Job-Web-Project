c6bc78f64107bdfab0f8919af503a430c35141f8
Titivat <titivatmo@gmail.com>
2020-12-21 19:34:15 +0700

    update: find job page

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,6 +1,7 @@
 import './SearchBar.css';
 
-function SearchBar(){
+function SearchBar( props ){
+    const { seach } = props
 
     return(
         <div className="searchbar-container">
@@ -11,7 +12,7 @@ function SearchBar(){
                 <div className="searchbar-search-container">
                     <p className="seachbar-text-color">Seach here:</p>
                     <p className="seachbar-button-style">
-                        <input className="serach-style" type="text" maxlength="20" size="40" />
+                        <input onChange={ seach } className="serach-style" type="text" maxlength="20" size="40" />
                     </p>
                 </div>
 
@@ -19,22 +20,22 @@ function SearchBar(){
                     <p className="seachbar-text-color">Job Type:</p>
                     <p className="seachbar-button-style">
                         <select className="seachbar-droup-box">
-                            <option value="item-1">Item 1</option>
-                            <option value="item-2">Item 2</option>
-                            <option value="item-3">Item 3</option>
-                            <option value="item-4">Item 4</option>
+                            <option value="Full-time">Full-time</option>
+                            <option value="Part-time">Part-time</option>
+                            <option value="Contract">Contract</option>
+                            <option value="Internship">Internship</option>
+                            <option value="Temporary">Temporary</option>
                         </select>
                     </p>
                 </div>
 
                 <div className="searchbar-search-container">
-                    <p className="seachbar-text-color">Company:</p>
+                    <p className="seachbar-text-color">Time:</p>
                     <p className="seachbar-button-style">
                         <select className="seachbar-droup-box">
-                            <option value="item-1">Item 1</option>
-                            <option value="item-2">Item 2</option>
-                            <option value="item-3">Item 3</option>
-                            <option value="item-4">Item 4</option>
+                            <option value="24">Past 24 hours</option>
+                            <option value="7">Past Week</option>
+                            <option value="30">Past Month</option>
                         </select>
                     </p>
                 </div>
@@ -43,10 +44,10 @@ function SearchBar(){
                     <p className="seachbar-text-color">Location:</p>
                     <p className="seachbar-button-style">
                         <select className="seachbar-droup-box">
-                            <option value="item-1">Item 1</option>
-                            <option value="item-2">Item 2</option>
-                            <option value="item-3">Item 3</option>
-                            <option value="item-4">Item 4</option>
+                            <option value="Bangkok">Bangkok</option>
+                            <option value="Sathon">Sathon</option>
+                            <option value="Watthana">Watthana</option>
+                            <option value="Phra Nakhon">Phra Nakhon</option>
                         </select>
                     </p>
                 </div>
