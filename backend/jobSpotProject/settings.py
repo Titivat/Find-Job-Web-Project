de5ce6838c957978a331efc341dea6b760aeb885
pollakit <60090031@kmitl.ac.th>
2020-12-25 12:32:42 +0700

    setting

--- backend/jobSpotProject/settings.py
+++ backend/jobSpotProject/settings.py
@@ -56,14 +56,19 @@ INSTALLED_APPS = [
     'positions.apps.PositionsConfig'
 ]
 
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.IsAuthenticated',
+#     ],
+#     'DEFAULT_AUTHENTICATION_CLASSES': [
+#         'rest_framework.authentication.SessionAuthentication',
+#         'rest_framework.authentication.TokenAuthentication',
+#     ],
+# }
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
-    ],
+        'rest_framework.permissions.AllowAny',
+    ]
 }
 
 
