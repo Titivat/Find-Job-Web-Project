0bc0ec9ca30fa3c61803d4a6aab6b52550a5ff09
pollakit <60090031@kmitl.ac.th>
2020-12-25 12:40:29 +0700

    get all post in same forum

--- backend/jobSpotProject/settings.py
+++ backend/jobSpotProject/settings.py
@@ -56,20 +56,21 @@ INSTALLED_APPS = [
     'positions.apps.PositionsConfig'
 ]
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PERMISSION_CLASSES': [
-#         'rest_framework.permissions.IsAuthenticated',
-#     ],
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'rest_framework.authentication.SessionAuthentication',
-#         'rest_framework.authentication.TokenAuthentication',
-#     ],
-# }
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
+# REST_FRAMEWORK = {
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.AllowAny',
+#     ]
+# }
 
 
 MIDDLEWARE = [
--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -3,6 +3,6 @@ from .views import PostList, PostDetail, PostFromSameForumList
 
 urlpatterns = [
     path('', PostList.as_view(), name='Post_list'),
-    path('<forum>', PostFromSameForumList.as_view(), name='Same_Forum_list'),
+    path('/forum/<forum>', PostFromSameForumList.as_view(), name='Same_Forum_list'),
     path('<int:pk>/', PostDetail.as_view(), name='Post_detail'),
 ]
